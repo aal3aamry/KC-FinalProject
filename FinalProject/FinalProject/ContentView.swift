a0043b9b90e5425f60096542dda546c4215b88c4
aal3aamry <abdulrahman123aaa@gmail.com>
2022-08-29 17:02:05 +0300

    firstPush

---
+++ FinalProject/FinalProject/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FinalProject
+//
+//  Created by عبدالرحمن العامري on 27/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+        Text("Hello, world!")
+            .padding()
+            
+            Text("hi")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
